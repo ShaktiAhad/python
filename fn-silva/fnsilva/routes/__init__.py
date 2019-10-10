24f7ce765817b42156aea597dee6d642843d6982
ShaktiAhad <ahadnoor80@gmail.com>
2019-10-10 10:03:05 +0900

    delete fn-silva

--- fn-silva/fnsilva/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from fnsilva.routes.api import silva_api
-
-
-def init_app(app):
-    app.register_blueprint(silva_api, url_prefix='/')
--- fn-silva/run.py
+++
@@ -1,8 +0,0 @@
-from fnsilva import create_app
-
-
-create_app().run(
-    host='0.0.0.0',
-    debug=True,
-    port=8080
-)
